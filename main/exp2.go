1ccb8a4c2ccc959071aeff9c866e32047411761f
yeqwep <38267288+yeqwep@users.noreply.github.com>
2024-07-08 00:54:13 +0900

    init

---
+++ main/exp2.go
@@ -0,0 +1,30 @@
+components {
+  id: "exp"
+  component: "/main/exp.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "exp_fx"
+  component: "/assets/fx/exp2.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
